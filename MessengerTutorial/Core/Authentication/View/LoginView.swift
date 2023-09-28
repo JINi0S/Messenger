92a013f8b33c35b80c5c68ff4395904fe394db40
radin <dlwlsgml8847@gmail.com>
2023-09-28 14:14:52 +0900

    Initial Commit

---
+++ MessengerTutorial/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,109 @@
+//
+//  LoginView.swift
+//  MessengerTutorial
+//
+//  Created by Lee Jinhee on 2023/09/26.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                Image("Messenger")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150,height: 150)
+                    .padding()
+                
+                VStack {
+                    TextField("Enter your email", text: $email)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 8)
+                    TextField("Enter your password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(10)
+                        .padding(.horizontal, 8)
+                }.frame(maxWidth: .infinity)
+                
+                Button {
+                    //find password
+                } label: {
+                    Text("Forget Password")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.top)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                Button {
+                    //Login
+                } label: {
+                    Text("Login")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 44)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                        .padding(.vertical)
+                }
+                
+
+                HStack {
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                    Text("or")
+                        .font(.footnote)
+                        .fontWeight(.semibold )
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                } .foregroundColor(.gray)
+                
+                HStack {
+                    Image("Facebook")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                    Text("Continue with Facebook")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.blue)
+                }
+                .padding(.top, 8)
+                Spacer()
+                Divider()
+                
+                NavigationLink {
+                    
+                } label: {
+                    HStack {
+                        Text("Dont'have an account?")
+                        Text("Sign Up")
+                        
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                }
+                .padding(.vertical)
+                
+            }.padding(16)
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ MessengerTutorial/MessengerTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerTutorialApp.swift
+//  MessengerTutorial
+//
+//  Created by Lee Jinhee on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct MessengerTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
